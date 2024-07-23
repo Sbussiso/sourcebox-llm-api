5ccb52101c0f37fb3396609e5d74ceaa16ee4635
sbussiso <sbussiso321@gmail.com>
2024-07-23 16:22:16 +0000

    removed uneccessary files and dependencies

--- hug_test.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-def sentiment_analysis(text: str) -> str:
-    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-    classifier = pipeline("sentiment-analysis", model=model_name)
-    result = classifier(text)
-    return result[0]
